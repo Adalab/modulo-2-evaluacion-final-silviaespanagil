0764dd1057971439818cf1e172044c28e03d689f
silviaespanagil <silviaespanagil@gmail.com>
2021-05-22 09:16:58 +0200

    Add series img fetch: works until a serie image=null

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ console.log(":D");
 [] El resultado de la API que me interesa es: imagen de la serie y título.
 [] Si la API no tiene imagen debo colocar una por default * https://via.placeholder.com/210x295/ffffff/666666/?text=TV * 
 -- root URL http://api.tvmaze.com
--- show search url /search/shows?q=:query
+-- show search url /search/shows?q=query
 -- important data: name and image.medium
 */
 const searchButton = document.querySelector(".js-buttonSearch");
@@ -24,7 +24,9 @@ function searchShow() {
       console.log(data[0]);
       for (let i = 0; i < seriesData.length; i++) {
         const seriesName = data[i].show.name;
+        const seriesImg = data[i].show.image.medium;
         console.log(seriesName); //working
+        console.log(seriesImg); //pseudo works...if not image console error of "cannot read property of null"
       }
     });
 }
