df445fbe1c48585eb3c25a4d266dab9c04f6def5
silviaespanagil <silviaespanagil@gmail.com>
2021-05-22 11:19:59 +0200

    Unify searchlist js in fetch for correct html painting

--- src/js/01 - fetch.js
+++ src/js/01 - fetch.js
@@ -16,9 +16,11 @@ let seriesData = [];
 function searchShow() {
   const userSearch = document.querySelector(".js-searchBox");
   const searchName = userSearch.value;
+  const searchURL = "http://api.tvmaze.com/search/shows?q=" + searchName;
   const imageDefault =
     "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
-  fetch("http://api.tvmaze.com/search/shows?q=" + searchName)
+
+  fetch(searchURL)
     .then((response) => response.json())
     .then((data) => {
       seriesData = data;
@@ -28,6 +30,30 @@ function searchShow() {
           data[i].show.image === null
             ? imageDefault
             : data[i].show.image.medium;
+
+        function createSubtitle() {
+          const searchSubtitle = document.querySelector(".js-resultArea");
+          const searchSubtitleContent = document.createTextNode(
+            "Estas son las series que coinciden con tu búsqueda"
+          );
+          searchSubtitle.appendChild(searchSubtitleContent);
+        }
+
+        function createList() {
+          const imgEl = document.createElement("img");
+          imgEl.src = seriesImg;
+          imgEl.alt = "Poster de serie";
+          const seriesNameEl = document.createElement("h3");
+          const seriesNameContent = document.createTextNode(seriesName);
+          seriesNameEl.appendChild(seriesNameContent);
+          const ul = document.querySelector(".js-searchResult");
+          const resultLi = document.createElement("li");
+          ul.appendChild(resultLi);
+          resultLi.appendChild(imgEl);
+          resultLi.appendChild(seriesNameEl);
+        }
+
+        createList();
       }
     });
 }
