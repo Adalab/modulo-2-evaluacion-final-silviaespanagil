3070cd3d3ed0f182d23a52e9108483d2c8ecf332
silviaespanagil <silviaespanagil@gmail.com>
2021-05-22 11:26:48 +0200

    Refactor fetch.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
 "use strict";
-console.log(":D");
 
-/* [x]Usuario debe poder buscar
-[x] API responde con resultado
-[x] Para que api responda debo enviar URL personalizada
-[x] El resultado de la API que me interesa es: imagen de la serie y título.
-[x] Si la API no tiene imagen debo colocar una por default * https://via.placeholder.com/210x295/ffffff/666666/?text=TV * 
--- root URL http://api.tvmaze.com
--- show search url /search/shows?q=query
--- important data: name and image.medium
-*/
 const searchButton = document.querySelector(".js-buttonSearch");
-let seriesData = [];
 
 function searchShow() {
   const userSearch = document.querySelector(".js-searchBox");
@@ -23,8 +12,7 @@ function searchShow() {
   fetch(searchURL)
     .then((response) => response.json())
     .then((data) => {
-      seriesData = data;
-      for (let i = 0; i < seriesData.length; i++) {
+      for (let i = 0; i < data.length; i++) {
         const seriesName = data[i].show.name;
         let seriesImg =
           data[i].show.image === null
@@ -33,13 +21,14 @@ function searchShow() {
 
         function createList() {
           const imgEl = document.createElement("img");
-          imgEl.src = seriesImg;
-          imgEl.alt = "Poster de serie";
           const seriesNameEl = document.createElement("h3");
           const seriesNameContent = document.createTextNode(seriesName);
-          seriesNameEl.appendChild(seriesNameContent);
           const ul = document.querySelector(".js-searchResult");
           const resultLi = document.createElement("li");
+
+          imgEl.src = seriesImg;
+          imgEl.alt = "Poster de serie";
+          seriesNameEl.appendChild(seriesNameContent);
           ul.appendChild(resultLi);
           resultLi.appendChild(imgEl);
           resultLi.appendChild(seriesNameEl);
