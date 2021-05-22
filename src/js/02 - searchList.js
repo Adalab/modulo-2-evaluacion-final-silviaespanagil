b1a2f964e6024c1f48a3addb175948f4de133a04
silviaespanagil <silviaespanagil@gmail.com>
2021-05-22 10:30:44 +0200

    Add image element

--- src/js/02 - searchList.js
+++ src/js/02 - searchList.js
@@ -1,18 +1,30 @@
 /* Pintar resultado del fetch en listado*/
 
-function createSubti() {
-  //crear subtítulo
+//crear subtítulo
+function createSubtitle() {
   const searchSubtitle = document.querySelector(".js-resultArea");
   const searchSubtitleContent = document.createTextNode(
     "Estas son las series que coinciden con tu búsqueda"
   );
   searchSubtitle.appendChild(searchSubtitleContent);
 }
+
+//creo image
+function createImage() {
+  const img = document.createElement("img");
+  img.src = "https://memegenerator.net/img/instances/69209806.jpg";
+  img.alt = "Poster de serie"; //esto cambia por variable img
+}
+
+//creo nombreserie
+function createName() {}
 //creo lista con image y nombre serie
+
 function createList() {
   const ul = document.querySelector(".js-searchResult");
   const resultLi = document.createElement("li");
 
   console.log(resultLi);
 }
-paintResult();
+
+//hago handler con todo lo que se pinta el handler seria la li que llama a la  img y al titulo?
