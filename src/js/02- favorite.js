676ca9dad5840ceb4c148a6f574030479c112e80
silviaespanagil <silviaespanagil@gmail.com>
2021-05-22 12:09:45 +0200

    Make every possible list element clickable

--- src/js/02- favorite.js
+++ src/js/02- favorite.js
@@ -1,1 +1,12 @@
-/*
+/* Al hacer clic sobre una serie se "favoritea"
+[]Color de fondo y fuente se intercambian (toggle?)
+[]Agrega listado en parte izquierda con series seleccionadas
+[]Si la usuaria realiza otra búsqueda los favoritos no se pierden
+*/
+
+//hacer cada li clickable
+
+const serie = document.querySelectorAll(".js-results");
+serie.forEach((li) => {
+  li.addEventListener("click", makefavorite);
+});
