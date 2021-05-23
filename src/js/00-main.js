968a61143999128daf18aa95c14ff56f6b91b582
silviaespanagil <silviaespanagil@gmail.com>
2021-05-23 21:09:49 +0200

    Paint fav column

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -21,6 +21,7 @@ function search() {
       }
     });
   createList();
+  makeLiClickable();
 }
 
 //Paint HTML
@@ -35,6 +36,7 @@ function createList() {
         ? defaultImage
         : defaultImage; /*allSeries[i].image.medium;*/
 
+    const seriesId = allSeries[i].id;
     const resultLi = document.createElement("li");
     const imgEl = document.createElement("img");
     const seriesNameEl = document.createElement("h3");
@@ -43,6 +45,7 @@ function createList() {
     imgEl.alt = "`${allSeries[i].name}`";
     seriesNameEl.appendChild(seriesNameContent);
     ulResults.appendChild(resultLi);
+    resultLi.id = seriesId;
     resultLi.appendChild(imgEl);
     resultLi.appendChild(seriesNameEl);
     resultLi.classList.add("js-results");
@@ -52,13 +55,43 @@ function createList() {
 }
 
 //Make li clickable for favs
-
-const allSeriesCards = document.querySelectorAll(".js-results");
-for (const seriesCard of allSeriesCards) {
-  seriesCard.addEventListener("click", classFavorite);
+function makeLiClickable() {
+  const allSeriesCards = document.querySelectorAll(".js-results");
+  for (const seriesCard of allSeriesCards) {
+    seriesCard.addEventListener("click", favoriteShow);
+  }
 }
-
 //Favorite
+
+function favoriteShow(ev) {
+  //tomo el favorito por click
+  const allShows = ev.currentTarget;
+  allShows.classList.add("js-favorite");
+
+  //pinto el favorito en nueva columna
+  if (allShows.classList.contains("js-favorite")) {
+    const favSection = document.querySelector(".js-favArea");
+    const newUl = document.querySelector(".js-searchFavs");
+    //const erase = document.createElement("img");
+    const favLi = allShows;
+
+    //erase.src = "../assets/images/eliminate.png";
+    //erase.width = "30";
+
+    newUl.appendChild(favLi);
+    //favLi.appendChild(erase);
+
+    favSection.classList.remove("js-hidden");
+    newUl.classList.add("js-favoriteArea");
+
+    //guardo la información del fav para pushear nuevo array
+    const idSelected = allSeries.id;
+    const favSeriesInfo = allSeries.find(
+      (serie) => allSeries.id === allShows.id
+    );
+    console.log(favSeriesInfo);
+  }
+}
 
 //Remove form default
 function preventSubmit(event) {
