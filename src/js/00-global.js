a421e359c007e58105a68f7e19d3b00c4bf0564e
silviaespanagil <silviaespanagil@gmail.com>
2021-05-23 20:32:47 +0200

    Add create list function

--- src/js/00-global.js
+++ src/js/00-global.js
@@ -19,7 +19,25 @@ function search() {
         allSeries.push(series.show);
       }
     });
+  createList();
 }
 
+//Paint HTML
+
+function createList() {
+  const defaultImage =
+    "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+  const ulResults = document.querySelector(".js-searchResult");
+  for (let i = 0; i < allSeries.length; i++) {
+    let seriesImg =
+      allSeries[i].image === null
+        ? defaultImage
+        : defaultImage; /*allSeries[i].image.medium;*/
+    const resultLi = document.createElement("li");
+    resultLi.innerHTML += `<img src=${seriesImg}/>`;
+    resultLi.innerHTML += `<h3>${allSeries[i].name}</h3>`;
+    ulResults.appendChild(resultLi);
+  }
+}
 searchButton.addEventListener("click", search);
 console.log(allSeries);
