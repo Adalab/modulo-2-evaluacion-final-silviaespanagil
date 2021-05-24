3c6e3438326726200a72bbc94c449763daf4e031
silviaespanagil <silviaespanagil@gmail.com>
2021-05-24 14:49:47 +0200

    Add eventListener to delete icon

--- src/js/00-main.js
+++ src/js/00-main.js
@@ -5,7 +5,6 @@
 const searchButton = document.querySelector(".js-buttonSearch");
 const form = document.querySelector(".js-form");
 const ulResults = document.querySelector(".js-searchResult");
-
 const favUl = document.querySelector(".js-searchFavs");
 const defaultImage =
   "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
@@ -85,6 +84,7 @@ function favoriteShow(ev) {
       (idFavorite) => idFavorite.id !== seriesId
     );
   }
+
   paintFav();
   setLocalStorage();
 }
@@ -141,6 +141,15 @@ function getLocal() {
   paintFav();
 }
 
+//delete favs
+
+function deleteIcon() {
+  const deleteIcons = document.querySelectorAll("small");
+  for (const deleteIcon of deleteIcons) {
+    favCard.addEventListener("click", deleteFav);
+  }
+}
+
 //Reset ul with new search
 
 function resultReset() {
@@ -155,8 +164,7 @@ function preventSubmit(event) {
   event.preventDefault();
 }
 
-//handler de searchButton
-
+//handler de searchButtons
 searchButton.addEventListener("click", search);
 form.addEventListener("submit", preventSubmit);
 getLocal();
